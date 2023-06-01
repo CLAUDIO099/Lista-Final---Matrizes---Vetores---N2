cafe3ec08bc03c7bfcda0057bd9fef70f23586f0
Cláudio <claudio.junior09@aluno.ifce.edu.br>
2023-06-01 12:04:49 -0300

    Create questao23.c

---
+++ questao23.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int mes;
+    printf("Digite o número do mês (1-12): ");
+    scanf("%d", &mes);
+    switch (mes) {
+        case 1:
+            printf("Janeiro tem 31 dias.\n");
+            break;
+        case 2:
+            printf("Fevereiro tem 28 ou 29 dias.\n");
+            break;
+        case 3:
+            printf("Março tem 31 dias.\n");
+            break;
+        case 4:
+            printf("Abril tem 30 dias.\n");
+            break;
+        case 5:
+            printf("Maio tem 31 dias.\n");
+            break;
+        case 6:
+            printf("Junho tem 30 dias.\n");
+            break;
+        case 7:
+            printf("Julho tem 31 dias.\n");
+            break;
+        case 8:
+            printf("Agosto tem 31 dias.\n");
+            break;
+        case 9:
+            printf("Setembro tem 30 dias.\n");
+            break;
+        case 10:
+            printf("Outubro tem 31 dias.\n");
+            break;
+        case 11:
+            printf("Novembro tem 30 dias.\n");
+            break;
+        case 12:
+            printf("Dezembro tem 31 dias.\n");
+            break;
+        default:
+            printf("Mês inválido. Digite um número entre 1 e 12.\n");
+    }
+    return 0;
+}
